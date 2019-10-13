616f53eb5a84971d39222f2a519bb711a544d075
Roypc86 <roypc86@gmail.com>
2019-10-13 11:15:10 -0600

    llist v2

--- Tarea#2/Lista/llist.h
+++ Tarea#2/Lista/llist.h
@@ -15,10 +15,10 @@ class llnode{
 		// Constructor por omision.
 		llnode(){
 		};
-		
+
       // Inicializacion de los datos miembro.
 		llnode (const T& k, llnode<T> *w = NULL, llnode<T> *y = NULL):key(k), prev(w), next(y) {};
-		
+
 		~llnode(){
 
 		}
@@ -29,75 +29,90 @@ template <typename T>
 class llist{
 	public:
 		llist(){
-			nil=nullptr;
-			nil.next=nullptr;
-			nil.prev=nullptr;
-		};
+            nil=new llnode<T>;
+            nil->next=nil;
+            nil->prev=nil;
+        };
 			// Constructor (crea una lista vacia)
 
-		llist(const llist<T>& obj){ 
-			llnode<T>* copy= obj.nil.prev;
-			while(copy!=nullptr){
-				listInsert(copy);
-				copy=copy.prev;
+		llist(const llist<T>& obj){
+            nil=new llnode<T>;
+            nil->next=nil;
+            nil->prev=nil;
+
+			llnode<T>* copy= obj.nil->prev;
+            printf("Valor: %d \t prev: %d \t next: %d \n", copy->key, copy->prev->key, copy->next->key);
+
+			while(copy!=obj.nil){
+				listInsert(new llnode<T>(copy->key, copy->prev, copy->next));
+				copy=copy->prev;
+				printf("Valor: %d \t prev: %d \t next: %d \n", copy->key, copy->prev->key, copy->next->key);
 			}
+
+			nil->prev=obj.nil->prev;
+			nil->next=obj.nil->next;
+
 		};
 			// Constructor copia
 
 		~llist(){
+		    delete nil;
 		};
 			// Destructor (borra la lista)
-					
+
 		llnode<T>* listSearch(const T& k){
-			llnode<T>* search= nil.next;
-			while((search!=nil)&&(search.k != k)){	
-				search=search.next;
-			}
-			if(search==nil){
-				search=NULL;
-			}
+            llnode<T>* search= nil->next;
+            while((search!=nil)&&(search->key != k)){
+                    search=search->next;
+            }
+            if(search==nil){
+                search=NULL;
+                printf("Hola desde el NULL search \n");
+            }
 			return search;
 		};
-			// Busca la llave iterativamente. Si la encuentra, devuelve un 
+			// Busca la llave iterativamente. Si la encuentra, devuelve un
             // apuntador al nodo que la contiene; sino devuelve NULL.
-			
+
 		void listInsert(llnode<T>* x){
-			if(nil.next==nullptr){
-				nil.next= x;
-				nil.prev=x;
-				x.prev=nil;		
-				x.next=nil;
+			if(nil->next==nil){
+				nil->next= x;
+				nil->prev=x;
+				x->prev=nil;
+				x->next=nil;
 			}else{
-				x.prev= nil;
-				x.next=nil.next; 
-				x.next.prev=x;
-				nil.next=x;				
+				x->prev= nil;
+				x->next=nil->next;
+				x->next->prev=x;
+				nil->next=x;
 			}
 		};
 			// Inserta el nodo x en la lista.
-	
+
 		llnode<T>* listDelete(llnode<T>* x){
-			llnode<T>* erase= nil.next;
-			while((erase!=nil)&&(erase.k != x.k)){
-				erase=erase.next;
+			llnode<T>* erase= nil->next;
+			while((erase!=nil)&&(erase->key != x->key)){
+				erase=erase->next;
 			}
 			if(erase!=nil){
-				erase.prev.next=erase.next;
-				erase.next.prev=erase.prev;
+				erase->prev->next=erase->next;
+				erase->next->prev=erase->prev;
+			}else{
+                erase=NULL;
 			}
 			return erase;
 		};
 			// Saca de la lista la llave contenida en el nodo apuntado por x.
-			// Devuelve la direccion del nodo eliminado para que se pueda 
+			// Devuelve la direccion del nodo eliminado para que se pueda
 			// disponer de el.
-				
+
 		llnode<T> * getNil() const{
 			return this->nil;
 		};
 		// Devuelve el nodo centinela. (Para efectos de revision de la tarea).
-			
+
 		private:
-	
+
 		llnode<T> *nil;	    // nodo centinela
 
 };
